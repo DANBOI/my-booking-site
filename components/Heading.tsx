33fd6fec1532c83f0d41f1258e3a12ecb90a35d8
chen <cycdanielboi@gmail.com>
2023-05-28 18:03:17 +0900

    DB setting & create models(prisma)

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -9,8 +9,8 @@ type Props = {
 export default function Heading({ title, subtitle, center }: Props) {
   return (
     <div className={center ? "text-center" : "text-start"}>
-      <div className="text-2xl font-bold">{title}</div>
-      <div className="mt-2 font-light text-neutral-500">{subtitle}</div>
+      <p className="text-2xl font-bold">{title}</p>
+      <p className="mt-2 font-light text-neutral-500">{subtitle}</p>
     </div>
   );
 }
